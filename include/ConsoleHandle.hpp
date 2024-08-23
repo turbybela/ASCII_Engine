9df71e0037cdfddae3f1cfee72f9d4a903ff5a61
Turbybela <turbybela@gmail.com>
2024-08-23 23:06:36 +0200

    lil progress

--- include/ConsoleHandle.hpp
+++ include/ConsoleHandle.hpp
@@ -3,6 +3,8 @@
 #include "Dimensions.hpp"
 #include "Exceptions.hpp"
 
+#include <iostream>
+
 #if defined(_WIN32) || defined(_WIN64)
 #define WINDOWS
 #elif defined(__linux__)
@@ -23,4 +25,12 @@ class ConsoleHandle {
     Dim2D<int> GetSize();
     Dim2D<int> GetCursorPosition();
     void ClearScreen();
+
+    Dim2D<int> SetCursorPosition(Dim2D<int> pos);
+    Dim2D<int> SetCursorPosition(int x, int y);
+
+    Dim2D<int> PutChar(char chr);
+    Dim2D<int> PutSequence(const char* sequence, int lenght);
+    Dim2D<int> PutSequence(const char* str);
+
 };
--- src/ConsoleHandle.cpp
+++ src/ConsoleHandle.cpp
@@ -28,6 +28,23 @@ Dim2D<int> ConsoleHandle::GetCursorPosition(){
 void ConsoleHandle::ClearScreen(){
     throw NotImplementedException("LINUX_ClearScreen");
 }
+
+Dim2D<int> SetCursorPosition(Dim2D<int> pos){
+    throw NotImplementedException("LINUX_SetCursorPos DIM");
+}
+Dim2D<int> SetCursorPosition(int x, int y){
+    throw NotImplementedException("LINUX_SetCursorPos XY");
+}
+
+Dim2D<int> PutChar(char chr){
+    throw NotImplementedException("LINUX_PutChar");
+}
+Dim2D<int> PutSequence(const char* sequence, int lenght){
+    throw NotImplementedException("LINUX_PutSequence");
+}
+Dim2D<int> PutSequence(const char* str){
+    throw NotImplementedException("LINUX_PutSequence STR");
+}
 #endif
 
 // --------------------------------------------------------------------------------
@@ -62,4 +79,24 @@ void ConsoleHandle::ClearScreen(){
     system("cls");
 }
 
+Dim2D<int> ConsoleHandle::SetCursorPosition(Dim2D<int> pos){
+
+}
+Dim2D<int> ConsoleHandle::SetCursorPosition(int x, int y){
+    
+}
+
+Dim2D<int> ConsoleHandle::PutChar(char chr){
+    std::cout << chr;
+    return GetCursorPosition();
+}
+Dim2D<int> ConsoleHandle::PutSequence(const char* sequence, int lenght){
+    std::cout.write(sequence, lenght);
+    return GetCursorPosition();
+}
+Dim2D<int> ConsoleHandle::PutSequence(const char* str){
+    std::cout << str;
+    return GetCursorPosition();
+    
+}
 #endif
